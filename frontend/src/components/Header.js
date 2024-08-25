b0fd687391b3eaf89426616a4799c4d9a6999607
Emily Fang Bazar <emilyfbazar@gmail.com>
2024-08-24 20:04:40 -0700

    Created Header files for navigation header layout

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,30 @@
+// AUTHOR: Emily Bazar
+// PROGRAM: Header.js
+// DATE: 24 August 2024
+// PURPOSE: Renders the header of the landing page, including navigation links.
+// INPUT: N/A
+// PROCESS: Displays logo image and navigation links.
+// OUTPUT: Header component with logo and navigation links.
+
+import React from 'react';
+import './Header.css';
+import logo from '../assets/BGSLogo.png';
+
+
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="logo">
+                <img src={logo} alt="Buy Give Share Logo" />
+            </div>
+            <nav className="navigation">
+                <a href="#about">About</a>
+                <a href="#contact">Contact</a>
+                <a href="#login">Log In</a>
+                <a href="#joinus">Join Us</a>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
