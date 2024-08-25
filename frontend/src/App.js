2cbb87d08d15e4087a666a23e26db5ed54667b20
Emily Fang Bazar <emilyfbazar@gmail.com>
2024-08-24 20:09:31 -0700

    Added comments and styling to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,21 @@
+// AUTHOR: Emily Bazar
+// PROGRAM: App.js
+// DATE:    24 August 2024
+// PURPOSE: Main component that includes the landing page.
+// INPUT:   No external inputs.
+// PROCESS: Renders the LandingPage component. More components to be added.
+// OUTPUT:  Displays the landing page in the app.
+
 import React from 'react';
 import LandingPage from './components/LandingPage';
+import './App.css';
 
 function App() {
-  return (
-      <div className="App">
-        <LandingPage />
-      </div>
-  );
+    return (
+        <div className="App">
+            <LandingPage /> {/* Render the LandingPage component */}
+        </div>
+    );
 }
 
 export default App;
