5b1e9b7b1e0c7eb351c5f88f7ea41d971c7f6f09
Emily Fang Bazar <emilyfbazar@gmail.com>
2024-09-20 12:40:13 -0700

    Modified imports to correct file paths

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,14 +8,14 @@
 
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import LandingPage from './components/LandingPage/LandingPage';
-import AboutPage from "./components/AboutPage/AboutPage";
-import Header from "./components/General/Header";
-import ContactPage from "./components/ContactPage/ContactPage";
-import LogInPage from "./components/LogInPage/LogInPage";
-import JoinUsPage from "./components/JoinUsPage/JoinUsPage";
-import BusinessSignUp from "./components/SignUpPage/BusinessSignUp";
-import CharitySignUp from "./components/SignUpPage/CharitySignUp";
+import LandingPage from './components/pages/LandingPage/LandingPage';
+import AboutPage from "./components/pages/AboutPage/AboutPage";
+import Header from "./components/pages/General/Header";
+import ContactPage from "./components/pages/ContactPage/ContactPage";
+import LogInPage from "./components/pages/LogInPage/LogInPage";
+import JoinUsPage from "./components/pages/JoinUsPage/JoinUsPage";
+import BusinessSignUp from "./components/pages/SignUpPage/BusinessSignUp";
+import CharitySignUp from "./components/pages/SignUpPage/CharitySignUp";
 import './App.css';
 
 function App() {
